3accacc95d81b9fe5439c57d14169901b13e5fe9
Samuel Molendolff Teixeira <contact@samuelmteixeira.dev>
2024-12-03 12:15:34 -0300

    refactor: replace eks modulo local to remote

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -22,7 +22,7 @@ module "vpc" {
 }
 
 module "eks" {
-  source = "../../modules/eks"
+  source = "github.com/fiap-postech-36/eks-infrastructure?ref=v1.0.0"
 
   cluster_name      = var.project_name
   vpc_id            = module.vpc.vpc_id
