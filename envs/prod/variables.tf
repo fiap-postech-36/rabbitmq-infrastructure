558473fd1982abfb39bcebbc1aae187a77eb5b74
Samuel Molendolff Teixeira <contact@samuelmteixeira.dev>
2024-12-03 12:57:26 -0300

    fix: pod is missing

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -7,14 +7,17 @@ variable "aws_region" {
 variable "project_name" {
   description = "Project Name"
   type        = string
+  default = "rabbitmq-infra"
 }
 
 variable "RABBITMQ_DEFAULT_USER" {
   description = "Default user for RabbitMQ"
   type        = string
+  default = "fiap"
 }
 
 variable "RABBITMQ_DEFAULT_PASS" {
   description = "Default password for RabbitMQ"
   type        = string
+  default = "GpP36_Fiap@2024!"
 }
